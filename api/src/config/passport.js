891ba4ca6c9bbd2ad60970160ace31518815bac6
mateomohr02 <mohr.mateo@gmail.com>
2023-05-31 23:16:43 -0300

    added logic for login

---
+++ api/src/config/passport.js
@@ -0,0 +1,34 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+
+const User = require('../models/User');
+
+const passportConfig = (passport) => {
+  passport.use(
+    new LocalStrategy(
+      {
+        usernameField: 'email',
+        passwordField: 'password',
+      },
+      async (email, password, done) => {
+        // Lógica de autenticación
+      }
+    )
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findByPk(id);
+      done(null, user);
+    } catch (error) {
+      done(error);
+    }
+  });
+};
+
+module.exports = passportConfig;
--- api/src/models/User.js
+++ api/src/models/User.js
@@ -13,7 +13,7 @@ module.exports = (sequelize) => {
           type: DataTypes.STRING,
           allowNull: false
       },
-      mail: {
+      email: {
           type: DataTypes.STRING,
           allowNull: false,
           unique: true,
